dddaf4e5e73737a41b4b5748901347aab023fa56
phamxuanduong <122418190+phamxuanduong@users.noreply.github.com>
2025-09-12 15:55:13 +0700

    Create k8sGetConfig.groovy

---
+++ vars/k8sGetConfig.groovy
@@ -0,0 +1,21 @@
+def call(Map args = [:]) {
+  if (!args.namespace) error 'k8sGetConfig: thiếu "namespace"'
+  if (!args.configmap) error 'k8sGetConfig: thiếu "configmap"'
+  if (!(args.items instanceof Map) || args.items.isEmpty()) {
+    error 'k8sGetConfig: thiếu "items" (Map: key-trong-ConfigMap -> đường-dẫn-file-đích)'
+  }
+
+  String ns = args.namespace
+  String cm = args.configmap
+  Map items = args.items  // ví dụ: ['Dockerfile':'build/images/base/Dockerfile', '.env':'deploy/dev/.env']
+
+  items.each { key, destPath ->
+    if (!destPath) error "k8sGetConfig: path rỗng cho key '${key}'"
+    sh """#!/bin/bash -eu
+      dir=\$(dirname '${destPath}')
+      [ "\$dir" = "." ] || mkdir -p "\$dir"
+      kubectl get configmap '${cm}' -n '${ns}' \
+        -o go-template='{{index .data \\\"${key}\\\"}}' > '${destPath}'
+    """
+  }
+}
