11f2b75d1d2e5c85a16ab89a8876ceb937345054
phamxuanduong <122418190+phamxuanduong@users.noreply.github.com>
2025-09-12 16:39:48 +0700

    Update k8sGetConfig.groovy

--- vars/k8sGetConfig.groovy
+++ vars/k8sGetConfig.groovy
@@ -1,3 +1,4 @@
+// vars/k8sGetConfig.groovy
 def call(Map args = [:]) {
   if (!args.namespace) error 'k8sGetConfig: thiếu "namespace"'
   if (!args.configmap) error 'k8sGetConfig: thiếu "configmap"'
@@ -7,15 +8,16 @@ def call(Map args = [:]) {
 
   String ns = args.namespace
   String cm = args.configmap
-  Map items = args.items  // ví dụ: ['Dockerfile':'build/images/base/Dockerfile', '.env':'deploy/dev/.env']
+  boolean strict = (args.strict == true)   // nếu true: báo lỗi khi thiếu key
 
-  items.each { key, destPath ->
+  args.items.each { String key, String destPath ->
     if (!destPath) error "k8sGetConfig: path rỗng cho key '${key}'"
     sh """#!/bin/bash -eu
       dir=\$(dirname '${destPath}')
       [ "\$dir" = "." ] || mkdir -p "\$dir"
-      kubectl get configmap '${cm}' -n '${ns}' \
-        -o go-template='{{index .data \\\"${key}\\\"}}' > '${destPath}'
+      kubectl get configmap '${cm}' -n '${ns}' \\
+        ${ strict ? "--allow-missing-template-keys=false" : "" } \\
+        -o go-template='{{index .data "${key}"}}' > '${destPath}'
     """
   }
 }
